d2cf481d903e7190c7fdf081d62b40be52eed8cb
hikaru <0dv6ky5w3265v4b@gmail.com>
2020-04-29 23:18:15 +0900

    Added audio scrape batch

--- src/batches.py
+++ src/batches.py
@@ -2,6 +2,9 @@ from learn_word import models as word_models
 from account import models as account_models
 from tqdm import tqdm
 import logging
+import requests
+import random
+import time
 logger = logging.getLogger("app")
 
 
@@ -18,3 +21,21 @@ def calc_learn_order():
             )
             word_summary.order = -1 * point
             word_summary.save()
+
+def download(link, filename="/static/sounds/test.mp3"):
+    # print(f'Download {link}')
+    req = requests.get(link)
+
+    with open(filename, 'wb') as f:
+        f.write(req.content)
+
+def get_audio():
+    word_obj_list = word_models.EnglishWord.objects.filter(audio_path__contains='https://weblio')
+    for word_obj in tqdm(word_obj_list):
+        link = word_obj.audio_path
+        filename = f'/static/sounds/{word_obj.word}.mp3'
+        logger.debug(filename)
+        download(link=link, filename=filename)
+        word_obj.audio_path = filename
+        word_obj.save()
+        time.sleep(random.random()*10)
